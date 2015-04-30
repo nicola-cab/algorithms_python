0cabcb4b8dff6e2c6dde4cd6aa574f921c6a68fa
nicola cabiddu <nicolacabiddu@gmail.com>
2015-04-30 17:09:12 +0100

    trie
    finish implementation for get and put in trie

--- strings/tst.py
+++ strings/tst.py
@@ -10,15 +10,31 @@ class tst:
 
     class node:
         def __init__(self):
+            self.n = 0
             self.key = 0
             self.val = 0
             self.left, self.mid, self.right = None, None, None
 
     def __init__(self):
         self.root = None
+    
+    def size(self):
+        return self.n
+    
+    def is_empty(self):
+        return self.size() == 0
 
     def put(self, key, value):
         self.root = self.__put__(self.root, key, value, 0)
+    
+    def get(self, key):
+        node = self.__get__(self.root, key, 0)
+        if not node:
+            raise Exception("Key not found")
+        return node.val
+    
+    def contains(self, key):
+        return self.__get__(self.root, key, 0) != None
 
     def __put__(self, node, key, value, d):
         c = key[d]
@@ -33,7 +49,21 @@ class tst:
             node.mid = self.__put__(node.mid, key, value, d+1)
         else:
             node.val = value
-
+        return node
+    
+    def __get__(self, node, key, d):
+        if node == None:
+            return None
+        c = key[d]
+        if c < node.key:
+            return self.__get__(node.left, key, d)
+        elif c > node.key:
+            return self.__get__(node.right, key, d)
+        elif d < len(key)-1:
+            return self.__get__(node.mid, key, d+1)
+        else:
+            return node
+        
 if __name__ == "__main__":
     str = "hello"
     str1 = "world"
@@ -46,5 +76,9 @@ if __name__ == "__main__":
     t = tst()
     strs = [str, str1, str2, str3, str4, str5, str6]
     for s in strs:
-        print("Processing: ", s)
+        print("Putting: ", s)
         t.put(s, 10)
+        
+    for s in strs:
+        print("Searching for :", s)
+        print(t.contains(s))
